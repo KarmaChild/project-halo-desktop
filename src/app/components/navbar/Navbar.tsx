53cd59d45737bb6410646d88d1c8db623df36af1
Adithya Mattappily <74791365+KarmaChild@users.noreply.github.com>
2024-03-12 11:37:55 -0600

    setup page 2

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -2,10 +2,10 @@ import {NavbarJoinButton} from "@/app/components/navbar/NavbarJoinButton"
 
 export const Navbar = () => {
     return (
-        <div className="bg-black absolute top-10 flex justify-center items-center h-[75px] w-[510px] rounded-[20px]">
+        <div className="z-10 bg-black absolute top-10 flex justify-center items-center h-[75px] w-[510px] rounded-[20px]">
             <div className="flex items-center justify-between">
                 <NavbarJoinButton/>
-                <p className="text-white text-23 font-regular relative ml-36 z-10">Login</p>
+                <p className="text-white text-23 font-regular relative ml-36">Login</p>
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,52 @@ import {UserNameTextEntry} from "@/app/components/UserNameTextEntry/UserNameText
 import {UserNameSignUpButton} from "@/app/components/UserNameTextEntry/UserNameSignUpButton";
 
 export default function Home() {
-  return (
-      <main className="relative h-screen max-w-screen-lg mx-auto border-2 border-amber-950">
-          <div className="flex justify-center">
-              <Navbar/>
-          </div>
-          <div className="absolute top-[200px] left-10">
-              <p className="font-bold text-64">
-                  One Link<br/>
-                  One Mini Website<br/>
-                  All of You
-              </p>
-          </div>
-          <div className="absolute top-[500px] left-10 w-[785px]">
-              <p className="font-light text-30">
-                  Your platform for showcasing your finest work, sharing services info, and connecting with your customers across all socials. Claim your username now.
-              </p>
-          </div>
-          <div className="absolute top-[650px] left-10">
-              <UserNameTextEntry/>
-              <div className="ml-[380px] z-100">
-                  <UserNameSignUpButton/>
+
+    return (
+      <div className="overflow-y-auto h-screen">
+          <main className="max-w-[1280px] mx-auto border-2 border-amber-950">
+              <div className="flex justify-center">
+                  <Navbar/>
               </div>
-          </div>
-      </main>
+              {/* first page */}
+              <div className="relative h-screen">
+                  <div className="absolute top-[200px] left-10">
+                      <p className="font-bold text-64">
+                          One Link<br/>
+                          One Mini Website<br/>
+                          All of You
+                      </p>
+                  </div>
+                  <div className="absolute top-[500px] left-10 w-[785px]">
+                      <p className="font-light text-30">
+                          Your platform for showcasing your finest work, sharing services
+                          info, and connecting with your customers across all socials. Claim
+                          your username now.
+                      </p>
+                  </div>
+                  <div className="absolute top-[650px] left-10">
+                      <UserNameTextEntry/>
+                      <div className="ml-[380px]">
+                          <UserNameSignUpButton/>
+                      </div>
+                  </div>
+              </div>
+              {/* second page */}
+              <div className="relative h-screen mt-4 bg-black">
+                  <div className="absolute top-10 left-0 right-0">
+                      <div className="text-center w-[1090px] h-[137px] m-auto">
+                          <p className="text-white font-bold text-64">
+                              Lead generation for ✨beauty✨ professionals, oversimplified
+                          </p>
+                      </div>
+                  </div>
+                  <div className="absolute top-[500px] left-10 w-[785px]">
+                      <p className="text-white font-light text-30">
+                          More content for the second page.
+                      </p>
+                  </div>
+              </div>
+          </main>
+      </div>
   );
 }
