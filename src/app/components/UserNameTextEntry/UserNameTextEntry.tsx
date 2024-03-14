707e022f492cb6253502be901784f86f110d76ef
Adithya Mattappily <74791365+KarmaChild@users.noreply.github.com>
2024-03-13 18:48:41 -0600

    add states to page

--- src/app/components/UserNameTextEntry/UserNameTextEntry.tsx
+++ src/app/components/UserNameTextEntry/UserNameTextEntry.tsx
@@ -1,8 +1,21 @@
-export const UserNameTextEntry = () => {
+'use client'
+
+import React from "react";
+
+interface UserNameTextEntryProps {
+    onChange?: (value: string) => void;
+}
+export const UserNameTextEntry:React.FC<UserNameTextEntryProps> = ({ onChange }) => {
   return (
       <div className="bg-grey absolute flex justify-start items-center h-[70px] w-[360px] rounded-[20px] pl-6">
           <p className="font-light text-30 ml-0 ">appna.me/</p>
-          <input type="text" className="h-[60px] w-[200px] border-none bg-transparent outline-none text-30 placeholder-opacity-50 font-[300]" placeholder="username"/>
+          <input type="text"
+                 className="
+                 h-[60px] w-[200px] border-none bg-transparent outline-none text-30
+                 placeholder-opacity-50 font-[300]"
+                 placeholder="username"
+                 onChange={(e) => onChange?.(e.target.value)}
+          />
       </div>
   )
 }
--- src/app/components/navbar/NavbarJoinButton.tsx
+++ src/app/components/navbar/NavbarJoinButton.tsx
@@ -1,8 +1,22 @@
+'use client'
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export const NavbarJoinButton = () => {
-  return (
-      <div className="bg-white absolute flex justify-center items-center h-[40px] w-[140px] rounded-[10px]">
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/join'); // Use push for client-side navigation
+    };
+
+
+
+    return (
+      <div className="
+      bg-white absolute flex justify-center items-center h-[40px] w-[140px] rounded-[10px]
+      transition duration-300 hover-bg-grey cursor-pointer
+      "
+      >
           <p className="text-23 font-regular mr-1">Join</p>
           <Image src="/icons/CTA_Arrow.png"
                  width={13}
