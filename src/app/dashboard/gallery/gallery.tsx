3423f34a8df0312e05b1f10914f254f09385944c
Adithya Mattappily <adithya1bijukumar@gmail.com>
2024-04-13 12:05:52 -0600

    add display image on select

--- src/app/dashboard/gallery/gallery.tsx
+++ src/app/dashboard/gallery/gallery.tsx
@@ -1,18 +1,23 @@
 import {DefaultButton} from "@/app/components/Button/DefaultButton"
-import React, {ChangeEvent} from "react"
+import React, {ChangeEvent, useState} from "react"
+import Image from "next/image";
 
 
 export const Gallery = () => {
+    const [selectedImage, setSelectedImage] = useState<File | null>(null)
+    const [showSelectedImage, setShowSelectedImage] = useState<boolean>(false)
     const fileInputRef = React.useRef<HTMLInputElement>(null)
 
     const handleButtonClick = () => {
-        if (fileInputRef.current) {
-            fileInputRef.current.click()
-        }
+        if (fileInputRef.current) fileInputRef.current.click()
     }
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files![0]
+        if (file) {
+            setSelectedImage(file);
+            setShowSelectedImage(true);
+        }
         console.log("Selected file:", file)
     }
 
@@ -21,12 +26,45 @@ export const Gallery = () => {
             <div className="absolute w-[510px] h-[1115px]">
                 {/* Add picture button*/}
                 <div className="absolute top-[0px] w-full flex justify-center">
-                    <input type="file"
-                           ref={fileInputRef}
-                           style={{ display: "none" }}
-                           onChange={handleFileChange}
-                    />
-                    <DefaultButton text={"Add a Picture"} onClick={handleButtonClick}/>
+                    { showSelectedImage ? (
+                        <div className="flex justify-center items-center w-[430px] h-[240px] bg-grey rounded-[15px]">
+                            <Image
+                                className="absolute top-[15px]"
+                                src={URL.createObjectURL(selectedImage!)}
+                                width={160}
+                                height={160}
+                                alt={'image'}
+                            />
+                            <div className="absolute top-[190px] flex w-full justify-center items-center">
+                                <input type="file"
+                                       ref={fileInputRef}
+                                       style={{display: "none"}}
+                                       onChange={handleFileChange}
+                                />
+                                <button
+                                    className="w-[100px] h-[35px] bg-black rounded-[50px] text-white font-light text-16
+                                        hover-bg-black transition duration-300 mr-1"
+                                    onClick={handleButtonClick}
+                                >
+                                    replace
+                                </button>
+                                <button
+                                    className="w-[100px] h-[35px] bg-black rounded-[50px] text-white font-light text-16
+                                        hover-bg-black transition duration-300">
+                                    upload
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div>
+                            <input type="file"
+                                   ref={fileInputRef}
+                                   style={{display: "none"}}
+                                   onChange={handleFileChange}
+                            />
+                            <DefaultButton text={"Add a Picture"} onClick={handleButtonClick}/>
+                        </div>
+                    )}
                 </div>
                 {/* Add picture button*/}
             </div>
